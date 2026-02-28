ac1e0f944e1c8787014445f1b7027edb160a843c
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2026-02-28 18:35:54 +0300

    state engine ready (#4)

--- src/app.js
+++ src/app.js
@@ -4,8 +4,22 @@ class App {
 	routes = [
 		{path: "", view: MainView }
 	];
+	appState = {
+		favorites: []
+	};
 
 	constructor() {
+		// Способ 1: Вызов через объект
+		// app.route();  this = app ✅
+
+		// Способ 2: Переменная с функцией
+		// const func = app.route;
+		// func();  this = window (потеряли контекст) ❌
+
+		// Почему bind решает проблему:
+		// const boundFunc = app.route.bind(app); // Создаем НОВУЮ функцию
+		// boundFunc(); // this = app всегда ✅
+		// bind "запоминает" внутри новой функции, что this должен быть app
 		window.addEventListener('hashchange', this.route.bind(this));
 		this.route();
 	}
@@ -15,7 +29,7 @@ class App {
 			this.currentView.destroy();
 		}
 		const view = this.routes.find(r => r.path == location.hash).view;
-		this.currentView = new view();
+		this.currentView = new view(this.appState);
 		this.currentView.render();
 	}
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,68 @@
-import { AbstractView } from '../../common/view.js';
+import { AbstractView } from "../../common/view.js";
+import onChange from "on-change";
 
 export class MainView extends AbstractView {
-	constructor() {
-		super();
-		this.setTitle('Поиск книг');
-	}
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0,
+  };
 
-	render() {
-		const main = document.createElement('div');
-		main.innerHTML = 'Тест';
-		this.app.innerHTML = '';
-		this.app.append(main);
-	}
+  constructor(appState) {
+    super();
+    this.appState = appState; // this.appState = переданный appState
+    // onChange - это библиотека, которая следит за изменениями объекта.
+    // Она создает "обернутый" объект (прокси), который ловит любые изменения.
+    // 1. Создаем слежку за this.appState
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
+    // 				                                         ↑
+    //				                                      привязываем MainView
+    // 2. Теперь this.appState - это не обычный объект,
+    // а "прокси" (объект-обертка), который следит за изменениями
+
+    // 3. Когда вы меняете любое свойство в this.appState:
+    // this.appState.favorites = [...];  // какое-то изменение
+
+    // 4. onChange перехватывает изменение и вызывает appStateHook с параметрами:
+    //    - path: путь к измененному свойству ("favorites")
+    //    - value: новое значение
+    //    - previousValue: старое значение
+
+    //////////////////////////////////////////////////////////////////////
+    // Создаем стрелочную функцию на лету
+    // this.appState = onChange(this.appState, (path) => {
+    // this берется из окружающего контекста (конструктора)
+    //   this.appStateHook(path, value, previousValue);
+    // });
+
+    // this.appState = onChange(this.appState, this.appStateHook);
+    this.setTitle("Поиск книг");
+  }
+
+  //   appStateHook(path) {
+  //     if (path === "favorites") {
+  //       console.log(path);
+  //     }
+  //   }
+
+  appStateHook(path, value) {
+    if (path === "favorites") {
+      console.log(path, value); // path: favorites value: ['d']
+    }
+  }
+
+  //   Стрелочная функция автоматически привязывает this к экземпляру класса
+  //   appStateHook = (path) => {
+  //     // this всегда указывает на MainView
+  //     console.log(this.state); // ✅ работает
+  //   };
+
+  render() {
+    const main = document.createElement("div");
+    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
+    this.app.innerHTML = "";
+    this.app.append(main);
+    this.appState.favorites.push("d");
+  }
 }
