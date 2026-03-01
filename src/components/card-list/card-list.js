0b55b1d31aa510830b615b6293f802fcb912a3be
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2026-03-01 15:04:26 +0300

    card layout ready (#9)

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,25 +1,28 @@
 import { DivComponent } from '../../common/div-component';
+import { Card } from '../card/card';
 import { Loader } from '../loader/loader';
 import './card-list.css';
 
 export class CardList extends DivComponent {
-  constructor(appState, parentState) {
-    super();
-    this.appState = appState;
-    this.parentState = parentState;
-  }
+	constructor(appState, parentState) {
+		super();
+		this.appState = appState;
+		this.parentState = parentState;
+	}
 
-  render() {
-    if (this.parentState.loading) {
-      this.el.innerHTML = '';
-      this.el.appendChild(new Loader('Ищем книги...').render());
-      return this.el;
-    }
-    
-    this.el.classList.add('card_list');
-    this.el.innerHTML = `
-      <h1>Найдено книг – ${this.parentState.list.length}</h1>
-    `;
-    return this.el;
-  }
+	render() {
+		if (this.parentState.loading) {
+			this.el.innerHTML = ''
+            this.el.appendChild(new Loader('Ищем книги...').render());
+			return this.el;
+		}
+		this.el.classList.add('card_list');
+		this.el.innerHTML = `
+			<h1>Найдено книг – ${this.parentState.numFound}</h1>
+		`
+		for (const card of this.parentState.list) {
+			this.el.append(new Card(this.appState, card).render());
+		}
+		return this.el;
+	}
 }
