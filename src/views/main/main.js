4f708422694b9b314b4390b65af6cf151b7e93e7
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2026-03-01 10:40:38 +0300

    Stage 5 the first component (#5)
    * first component
    
    * first component ready

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,68 +1,37 @@
-import { AbstractView } from "../../common/view.js";
-import onChange from "on-change";
+import { AbstractView } from '../../common/view.js';
+import onChange from 'on-change';
+import { Header } from '../../components/header/header.js';
 
 export class MainView extends AbstractView {
-  state = {
-    list: [],
-    loading: false,
-    searchQuery: undefined,
-    offset: 0,
-  };
+	state = {
+		list: [],
+		loading: false,
+		searchQuery: undefined,
+		offset: 0
+	};
 
-  constructor(appState) {
-    super();
-    this.appState = appState; // this.appState = переданный appState
-    // onChange - это библиотека, которая следит за изменениями объекта.
-    // Она создает "обернутый" объект (прокси), который ловит любые изменения.
-    // 1. Создаем слежку за this.appState
-    this.appState = onChange(this.appState, this.appStateHook.bind(this));
-    // 				                                         ↑
-    //				                                      привязываем MainView
-    // 2. Теперь this.appState - это не обычный объект,
-    // а "прокси" (объект-обертка), который следит за изменениями
+	constructor(appState) {
+		super();
+		this.appState = appState;
+		this.appState = onChange(this.appState, this.appStateHook.bind(this))
+		this.setTitle('Поиск книг');
+	}
 
-    // 3. Когда вы меняете любое свойство в this.appState:
-    // this.appState.favorites = [...];  // какое-то изменение
+	appStateHook(path) {
+		if (path === 'favorites') {
+			console.log(path);
+		}
+	}
 
-    // 4. onChange перехватывает изменение и вызывает appStateHook с параметрами:
-    //    - path: путь к измененному свойству ("favorites")
-    //    - value: новое значение
-    //    - previousValue: старое значение
+	render() {
+		const main = document.createElement('div');
+		this.app.innerHTML = '';
+		this.app.append(main);
+		this.renderHeader();
+	}
 
-    //////////////////////////////////////////////////////////////////////
-    // Создаем стрелочную функцию на лету
-    // this.appState = onChange(this.appState, (path) => {
-    // this берется из окружающего контекста (конструктора)
-    //   this.appStateHook(path, value, previousValue);
-    // });
-
-    // this.appState = onChange(this.appState, this.appStateHook);
-    this.setTitle("Поиск книг");
-  }
-
-  //   appStateHook(path) {
-  //     if (path === "favorites") {
-  //       console.log(path);
-  //     }
-  //   }
-
-  appStateHook(path, value) {
-    if (path === "favorites") {
-      console.log(path, value); // path: favorites value: ['d']
-    }
-  }
-
-  //   Стрелочная функция автоматически привязывает this к экземпляру класса
-  //   appStateHook = (path) => {
-  //     // this всегда указывает на MainView
-  //     console.log(this.state); // ✅ работает
-  //   };
-
-  render() {
-    const main = document.createElement("div");
-    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
-    this.app.innerHTML = "";
-    this.app.append(main);
-    this.appState.favorites.push("d");
-  }
+	renderHeader() {
+		const header = new Header(this.appState).render();
+		this.app.prepend(header);
+	}
 }
