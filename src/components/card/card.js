de2a32de9f8878f57d22a6b58687133de4c10c1d
StanleyIG <104508407+StanleyIG@users.noreply.github.com>
2026-03-01 15:26:31 +0300

    adding to favorites ready (#10)

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,6 +8,16 @@ export class Card extends DivComponent {
 		this.cardState = cardState;
 	}
 
+	#addToFavorites() {
+		this.appState.favorites.push(this.cardState);
+	}
+
+	#deleteFromFavorites() {
+		this.appState.favorites = this.appState.favorites.filter(
+			b => b.key !== this.cardState.key
+		);
+	}
+
 	render() {
 		this.el.classList.add('card');
 		const existInFavorites = this.appState.favorites.find(
@@ -31,12 +41,21 @@ export class Card extends DivComponent {
 					<button class="button__add ${existInFavorites ? 'button__active' : ''}">
 						${existInFavorites 
 							? '<img src="/static/favorites.svg" />'
-							: '<img src="/static/favorite-white.svg" />'
+							: '<img src="/static/favorites-white.svg" />'
 						}
 					</button>
 				</div>
 			</div>
-		`
+		`;
+		if (existInFavorites) {
+			this.el
+				.querySelector('button')
+				.addEventListener('click', this.#deleteFromFavorites.bind(this));
+		} else {
+			this.el
+				.querySelector('button')
+				.addEventListener('click', this.#addToFavorites.bind(this));
+		}
 		return this.el;
 	}
 }
